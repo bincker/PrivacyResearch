c68d438a779412f7b5c5e82dfd3c6915ffd96762
ameyasanzgiri <ams76@buffalo.edu>
2014-08-11 15:58:19 -0700

    v1.2 by Ameya

---
+++ API_call2.py
@@ -0,0 +1,19 @@
+#!/bin/usr/python
+'''
+#	File Name : API_call2.py
+
+#	Purpose:
+
+#	Creation Date : 11-08-2014
+
+#	Last Modified: Mon 11 Aug 2014 02:40:29 PM PDT
+
+#	Created By : Ameya Sanzgiri
+
+#	Organization : McAfee Inc.
+
+'''
+
+import sys,time,os,subprocess,re, csv,ConfigParser,inspect
+
+
